743ef1e1bc22864b8e001007fd8a20a94a233763
randepukperesoh <mirgthebest@gmail.com>
2025-10-28 15:39:30 +0300

    fix

--- src/components/shared/backgrounds/rain/index.tsx
+++ src/components/shared/backgrounds/rain/index.tsx
@@ -1,8 +1,8 @@
+"use client";
 import dynamic from "next/dynamic";
 
 const MotionRain = dynamic(() => import("./Rain"), {
   ssr: false,
-  loading: () => <div>Loading Rain...</div>,
 });
 
 export default MotionRain;
